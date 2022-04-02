da66b7dd09aad7673f52190a0bbc4abb4edb09ad
janghyoseog <bestdevhyo1225@gmail.com>
2022-04-02 15:28:08 +0900

    feat: 데코레이터 패턴 - 예제 코드1

---
+++ src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,15 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.RealComponent;
+import org.junit.jupiter.api.Test;
+
+public class DecoratorPatternTest {
+
+    @Test
+    void noDecorator() {
+        RealComponent realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+}
