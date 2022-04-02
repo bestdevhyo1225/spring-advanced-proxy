9850bb8e7cb5d5ac4b730bdfb09299966885ca15
janghyoseog <bestdevhyo1225@gmail.com>
2022-04-02 14:01:05 +0900

    feat: 예제 프로젝트 만들기 V1

---
+++ src/main/java/hello/proxy/config/AppV1Config.java
@@ -0,0 +1,29 @@
+package hello.proxy.config;
+
+import hello.proxy.app.v1.OrderControllerV1;
+import hello.proxy.app.v1.OrderControllerV1Impl;
+import hello.proxy.app.v1.OrderRepositoryV1;
+import hello.proxy.app.v1.OrderRepositoryV1Impl;
+import hello.proxy.app.v1.OrderServiceV1;
+import hello.proxy.app.v1.OrderServiceV1Impl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 내부에 @Component가 붙어 있음. -> 컴포넌트 스캔 대상이 됨.
+public class AppV1Config {
+
+    @Bean
+    public OrderControllerV1 orderControllerV1() {
+        return new OrderControllerV1Impl(orderServiceV1());
+    }
+
+    @Bean
+    public OrderServiceV1 orderServiceV1() {
+        return new OrderServiceV1Impl(orderRepositoryV1());
+    }
+
+    @Bean
+    public OrderRepositoryV1 orderRepositoryV1() {
+        return new OrderRepositoryV1Impl();
+    }
+}
